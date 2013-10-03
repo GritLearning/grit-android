86b53c5382ec80ba6e4412b595a623ad1a728e43
Eoin Kelly <eoin@rabidtech.co.nz>
2013-10-04 10:32:54 +1300

    Remove unecessary build files

--- bin/Grit-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/lenz/work/projects/grit/grit-phonegap/bin/Grit-debug-unaligned.apk : \
-/Users/lenz/work/projects/grit/grit-phonegap/bin/Grit.ap_ \
-/Users/lenz/work/projects/grit/grit-phonegap/bin/classes.dex \
-/Users/lenz/work/projects/grit/grit-phonegap/src/com/GritLearning/plugin/.vimbk/GritLauncher.java.vimbk \
-/Users/lenz/work/projects/grit/grit-phonegap/src/com/GritLearning/plugin/.vimbk/Launcher.java.vimbk \
-/Users/lenz/work/projects/grit/grit-phonegap/libs/cordova-2.5.0.jar \
